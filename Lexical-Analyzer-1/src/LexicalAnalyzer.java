50e898cfc653cc3456f7867a3925eb850b6dfbc5
Francesco Garofalo <francescogarofalo182@live.com>
2018-11-02 09:22:26 +0100

    Modifiche

--- Lexical-Analyzer-1/src/LexicalAnalyzer.java
+++ Lexical-Analyzer-1/src/LexicalAnalyzer.java
@@ -20,51 +20,35 @@ public class LexicalAnalyzer {
 	public static String ritorna;
 	public static String token = "";
 	public static int lTesto = 0;
-	public static ArrayList tokens = new ArrayList<Token>();
+	public static ArrayList <Token> tokens = new ArrayList<Token>();
+	public static ArrayList <Token> tabellasimboli = new ArrayList<Token>();
 	public static int i = 0;
 	public static int id = 1;
 
 	public static ArrayList getToken(String lessema){
-		while(i<=lessema.length()) {
-		token = findToken(lessema);
-		Token to = new Token();
-		if(token == "if") {
-			token = "if";
-			to.setId(token);
-			to.setAttribute("Keyword");
-		} else if (token == "then") {
-			token = "then";
-			to.setId(token);
-			to.setAttribute("Keyword");
-		} else if (token == "else") {
-			token = "else";
-			to.setId(token);
-			to.setAttribute("Keyword");
-		} else if (token == "while") {
-			token = "while";
-			to.setId(token);
-			to.setAttribute("Keyword");
-		} else if (token == "for") {
-			token = "for";
-			to.setId(token);
-			to.setAttribute("Keyword");
-		}
-		else {
-			for(int k=0; k<tokens.size(); k++) {
-				Token t = new Token();
-				if(t.getAttribute()== token) {
-					tokens.add(t);
-				} else {
-					t.setId(""+id);
-					t.setAttribute(token);
-					id++;
-				}
+		lTesto = lessema.length()-1;
+		System.out.println(lTesto);
+		while(i<lTesto) {
+			System.out.println("Chiamo token");
+			token = findToken(lessema);
+			boolean isPres = false;
+			if(token == "") {
+
+			}else {
+				Token to = new Token();
+				to.setId(""+id);
+				to.setAttribute(token);
+				tokens.add(to);
+				id++;	
 			}
+			token = "";
 		}
-		tokens.add(token);
-		token = "";
-		}
+
 		return tokens;
+	}
+
+	public static void addToArray() {
+
 	}
 
 	/*
@@ -72,7 +56,6 @@ public class LexicalAnalyzer {
 	 */
 	public static String findToken(String testo) {
 		int stato = 0;
-		lTesto = testo.length();
 		//int backword = 0;
 		char c;
 		boolean active = true;
@@ -89,18 +72,35 @@ public class LexicalAnalyzer {
 					token = token + c;
 					log.info("Case 0: isLetter");
 					stato = 1;
-				} else {
-					System.out.println("Case 0 else 0");
-				}
-				//Controllo lunghezza testo
-				if(i <= lTesto) {
-					i++;
-				} else {
-					active = false;
-					System.out.println("case 0 fine lunghezza");
-				}
-				break;
+					if(i < lTesto) {
+						i++;
+						System.out.println("i++");
+						//CASE 4
+					} else if(Character.isDigit(c)) {
+						token = token + c;
+						log.info("Case 0: isDigit");
+						stato = 4;
+						if(i < lTesto) {
+							i++;
+							System.out.println("i++");
 
+						} else {
+							active = false;
+							System.out.println("case 0 fine lunghezza");
+						}
+					} else {
+						System.out.println("Case 0 not recognized");
+						active = false;
+						if(i <lTesto) {
+							i++;
+							System.out.println("i++");
+						}
+						break;
+
+					}
+					//Controllo lunghezza testo
+					break;
+				}
 
 				/*
 				 * CASE 1:
@@ -110,15 +110,16 @@ public class LexicalAnalyzer {
 				if((Character.isLetter(c)) | (Character.isDigit(c))) {
 					token = token + c;
 					log.info("Case 1: isLetter OR isDigit");
-				} else {
-					i--;
-				}
-				//Controllo lunghezza testo
-				if(i <= lTesto) {
-					i++;
+					if(i < lTesto) {
+						System.out.println(i);
+						i++;
+					} else {
+						active = false;
+						System.out.println("case 0 fine lunghezza");
+					}
 				} else {
 					active = false;
-					System.out.println("case 0 fine lunghezza");
+					break;
 				}
 				break;
 
@@ -127,12 +128,55 @@ public class LexicalAnalyzer {
 				if(Character.isDigit(c)) {
 					token = token + c;
 					log.info("Case 2: isDigit");
-				}else {
-					
-					
+					if(i < lTesto) {
+						System.out.println("pass");
+						System.out.println(i);
+						i++;
+					} else {
+						active = false;
+						System.out.println("case 0 fine lunghezza");
+					}
+				} else {
+					active = false;
+					break;
 				}
-				
-				
+				break;
+
+			case 3:
+				c = testo.charAt(i);
+				if(Character.isDigit(c)) {
+					token = token + c;
+					log.info("Case3: ");
+				} else {
+					i--;
+				}
+				//Controllo lunghezza testo
+				if(i <= lTesto) {
+					i++;
+				} else {
+					active = false;
+					System.out.println("case 3 fine lunghezza");
+				}
+				break;
+
+
+			case 4:
+				c = testo.charAt(i);
+				if(Character.isDigit(c)) {
+					token = token + c;
+					log.info("Case4: ");
+				} else {
+					i--;
+				}
+				//Controllo lunghezza testo
+				if(i <= lTesto) {
+					i++;
+				} else {
+					active = false;
+					System.out.println("case 3 fine lunghezza");
+				}
+				break;
+
 				/*
 				 * Default: Not WORK
 				 */
--- Lexical-Analyzer-1/src/Tester.java
+++ Lexical-Analyzer-1/src/Tester.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 
 public class Tester {
 	
-	
+	public static ArrayList<Token> token = new ArrayList<Token>();
 	public static void main (String[] args) {
 		String lessema = "fdsfs fsfds";
-		System.out.println(LexicalAnalyzer.getToken(lessema));
+		token = LexicalAnalyzer.getToken(lessema);
+		for(int i=0; i<token.size(); i++) {
+			System.out.println(token.get(i).toString());
+		}
 	}
 }
